62fd200cc92a3f5d8e2254fa4a8a7a074d9833d3
Leguilcher Yanis <yanis_leguilcher@ens.univ-artois.fr>
2025-03-18 19:36:43 +0100

    new configuration and editor

--- .utmp/RelWithDebInfo/5gs3054n/arm64-v8a/cmake_install.cmake
+++ .utmp/RelWithDebInfo/5gs3054n/arm64-v8a/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/yanis/Desktop/Projet/Unity/6000.0.41f1/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/yanis/Desktop/Projet/Unity/6000.0.42f1/Editor/Data/PlaybackEngines/AndroidPlayer/NDK/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
